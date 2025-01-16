0529813a0fa8daecd75afbb4ab0a3ad86e48bbaa
Dustin Whited <dgwhited@gmail.com>
2025-01-16 16:29:02 -0600

    output vpc_id

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,8 @@ output "ecr_repository_url" {
 output "github_token_secret_arn" {
   value = aws_secretsmanager_secret.github_token.arn
 }
+
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = module.vpc.vpc_id
+}
