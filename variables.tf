2ce5b2d09264ad3648bca8911b5a0d10fa5a1cd9
Dustin Whited <dgwhited@gmail.com>
2025-01-16 15:37:08 -0600

    feat: add custom policy attachment to task

--- variables.tf
+++ variables.tf
@@ -1,49 +1,60 @@
-variable "image_tag" {
-  type        = string
-  default     = "latest"
-  description = "Leave as latest for initial deployment. Then update to set the correct image tag (not URI)."
-}
-
-variable "num_runners" {
-  type        = number
-  default     = 0
-  description = "Set to 0 for initial deployment. Then scale once the runner image is available in the repo."
-}
-
-variable "github_org" {
-  type        = string
-  description = "organization"
-}
-
-variable "cpu" {
-  type        = string
-  description = "CPU allocation for runner containers. Be sure to match with memory!"
-  default     = "4096"
-}
-
-variable "memory" {
-  type        = string
-  description = "Memory allocation for runner containers. Be sure to match with CPU!"
-  default     = "8192"
-}
-
-variable "ecr_repo_name" {
-  type        = string
-  description = "Name for the ECR repo that will be created to hold runner custom image"
-  default     = "github-runner-image"
+variable "custom_task_policies" {
+  description = "List of IAM policy ARNs to attach to the task role"
+  type        = list(string)
+  default     = []
 }
 
 variable "stack_name" {
+  description = "Name of the stack"
   type        = string
-  description = "Name of the stack/deployment"
 }
 
 variable "vpc_cidr" {
+  description = "CIDR block for VPC"
   type        = string
-  description = "CIDR block for the VPC to split into two equal parts for subnets."
+  default     = "10.0.0.0/16"
+}
+
+variable "ecr_repo_name" {
+  description = "Name of the ECR repository"
+  type        = string
 }
 
 variable "environment" {
+  description = "Environment name"
   type        = string
-  description = "Environment tag for the runner."
+}
+
+variable "github_org" {
+  description = "GitHub organization name"
+  type        = string
+}
+
+variable "image_tag" {
+  description = "Docker image tag to use"
+  type        = string
+}
+
+variable "cpu" {
+  description = "CPU units for the task"
+  type        = number
+  default     = 256
+}
+
+variable "memory" {
+  description = "Memory for the task in MB"
+  type        = number
+  default     = 512
+}
+
+variable "num_runners" {
+  description = "Number of runners to maintain"
+  type        = number
+  default     = 1
+}
+
+variable "tags" {
+  description = "Tags to apply to resources"
+  type        = map(string)
+  default     = {}
 }
